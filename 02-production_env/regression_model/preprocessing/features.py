35c492ae0073f3393eec94c0a950ba244199c75a
Mukesh Manral <mukeshmanral777@gmail.com>
2023-01-20 23:32:57 +0530

    Production code - more code to add
    pussing production code

---
+++ 02-production_env/regression_model/preprocessing/features.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from typing import List
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
+    """
+    Temporal elapsed time transformer.
+    """
+    def __init__(self, variables: List[str], reference_variable: str):
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
+        self.variables = variables
+        self.reference_variable = reference_variable
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        #need this step to fit sklearn pipeline
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        # so that I do not over-write original dataframe
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[self.reference_variable] - X[feature]
+        return X
+
+    
+
+class Mapper(BaseEstimator, TransformerMixin):
+    """
+    Categorical variable mapper.
+    """
+    def __init__(self, variables: List[str], mappings: dict):
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
+        self.variables = variables
+        self.mappings = mappings
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        #need the fit statement to accomodate sklearn pipeline
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
+        return X
